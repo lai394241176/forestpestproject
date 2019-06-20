0a9d8d0181466bd7bf1e31cd7080cf48d4f0c795
sanYuWenChe <wangche1122@gmail.com>
2019-06-20 16:30:13 +0800

    v1.0

--- src/main/java/com/lovo/system/dao/IProfessorDao.java
+++ src/main/java/com/lovo/system/dao/IProfessorDao.java
@@ -1,5 +1,10 @@
 package com.lovo.system.dao;
 
+import com.lovo.system.bean.ProfessorBean;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * @author che
  * @title: IProfessorDao 专家持久接口
@@ -9,5 +14,59 @@ package com.lovo.system.dao;
  */
 public interface IProfessorDao {
 
+    //==================增加=======================
 
+    /**
+     * 添加专家
+     *
+     * @param professorBean 专家实体类
+     * @return 受影响的行数
+     */
+    int insertProfessor(ProfessorBean professorBean);
+
+    //==================删除=======================
+
+    /**
+     * 按专家id 删除专家
+     *
+     * @param professorId 专家id
+     * @return 受影响的行数
+     */
+    int deletProfessorByid(int professorId);
+
+
+    //==================查询=======================
+
+    /**
+     * 按专家id 查询专家
+     *
+     * @param professorId 专家id
+     * @return 专家实体类
+     */
+    ProfessorBean findProessorById(int professorId);
+
+    /**
+     * 查询所有专家
+     *
+     * @return 专家集合
+     */
+    List<ProfessorBean> findAllProfessor();
+
+    /**
+     * 条件查询专家
+     *
+     * @param itemMap 条件map
+     * @return 符合条件专家集合
+     */
+    List<ProfessorBean> findProfessorByItem(Map itemMap);
+
+    //==================修改=======================
+
+    /**
+     * 修改专家信息
+     *
+     * @param professorUpdateMap 专家修改信息map
+     * @return 受影响的行数
+     */
+    int updateProfessor(Map professorUpdateMap);
 }
