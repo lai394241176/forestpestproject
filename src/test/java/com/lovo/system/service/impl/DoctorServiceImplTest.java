d1f3dc55cab7ebae5fbe29d04d88f7094df1b63a
sanYuWenChe <wangche1122@gmail.com>
2019-06-22 13:01:30 +0800

    完善专家业务接口

--- src/test/java/com/lovo/system/service/impl/DoctorServiceImplTest.java
+++ src/test/java/com/lovo/system/service/impl/DoctorServiceImplTest.java
@@ -9,7 +9,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -65,9 +67,26 @@ public class DoctorServiceImplTest {
 
     @Test
     public void findDoctorByItem() {
+        Map<String, String> map = new HashMap<>();
+        map.put("findByName", null);
+        map.put("findBySpeciality", null);
+        map.put("findByCompany", null);
+
+        List<SysDoctor> list = service.findDoctorByItem(map);
+        System.out.println(list);
     }
 
     @Test
     public void updateDoctor() {
+        Map<String, Object> updateMap = new HashMap<>();
+        updateMap.put("updateId", 3);
+        updateMap.put("updateTel", "123123123");
+        updateMap.put("updatePost", "123123123");
+        updateMap.put("updateCompany", "朗沃");
+        updateMap.put("updateEmail", "123123123");
+
+        service.updateDoctor(updateMap);
+        SysDoctor s = service.findDoctorById(3);
+        System.out.println(s);
     }
 }
